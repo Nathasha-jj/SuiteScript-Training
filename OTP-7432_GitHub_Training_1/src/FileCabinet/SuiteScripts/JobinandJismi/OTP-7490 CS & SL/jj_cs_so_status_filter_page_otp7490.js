813cb2f405f999b43c5f17d058590da6ca9038b8
Nathasha-jj <nathasha.vangana@jobinandjismi.com>
2024-08-08 11:32:49 +0530

    otp-7490-ci: To push

---
+++ OTP-7432_GitHub_Training_1/src/FileCabinet/SuiteScripts/JobinandJismi/OTP-7490 CS & SL/jj_cs_so_status_filter_page_otp7490.js
@@ -0,0 +1,217 @@
+/**
+ * @NApiVersion 2.x
+ * @NScriptType ClientScript
+ * @NModuleScope SameAccount
+ */
+define(['N/currentRecord', 'N/record', 'N/search','N/url'],
+/**
+ * @param{currentRecord} currentRecord
+ * @param{record} record
+ * @param{search} search
+ */
+function(currentRecord, record, search, url) {
+    
+    /**
+     * Function to be executed after page is initialized.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.mode - The mode in which the record is being accessed (create, copy, or edit)
+     *
+     * @since 2015.2
+     */
+    // function pageInit(scriptContext) {
+
+    // }
+
+    /**
+     * Function to be executed when field is changed.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     * @param {string} scriptContext.fieldId - Field name
+     * @param {number} scriptContext.lineNum - Line number. Will be undefined if not a sublist or matrix field
+     * @param {number} scriptContext.columnNum - Line number. Will be undefined if not a matrix field
+     *
+     * @since 2015.2
+     */
+    function fieldChanged(scriptContext) 
+    {
+        let currentRecord = scriptContext.currentRecord;
+        console.log(currentRecord);
+        let fieldId = scriptContext.fieldId;
+        console.log("Field Id",fieldId);
+        // let statusFilter = '';
+        // let customerFilter = '';
+        // let subsidiaryFilter = '';
+        // let departmentFilter = '';
+
+        let statusFilter = currentRecord.getValue('custpage_status');
+        let customerFilter = currentRecord.getValue('custpage_customer');
+        let subsidiaryFilter = currentRecord.getValue('custpage_subsidiary');
+        let departmentFilter = currentRecord.getValue('custpage_department');
+        // if(fieldId =='custpage_status')
+        // {
+        //     statusFilter=currentRecord.getValue('custpage_status'); // If we need to set single filter at a time
+        // }
+        // if(fieldId =='custpage_customer')
+        // {
+        //     customerFilter=currentRecord.getValue('custpage_customer');
+        // }
+        // if(fieldId =='custpage_subsidiary')
+        // {
+        //     subsidiaryFilter=currentRecord.getValue('custpage_subsidiary');
+        // }
+        // if(fieldId =='custpage_department')
+        // {
+        //     departmentFilter=currentRecord.getValue('custpage_department');
+        // }
+
+        if(fieldId=='custpage_status'||'custpage_customer'||'custpage_subsidiary'||'custpage_department')
+        {
+            document.location = url.resolveScript(
+            {
+                deploymentId: 'customdeploy_jj_sl_so_filterpage_otp7490',
+                scriptId: 'customscript_jj_sl_so_filterpage_otp7490',
+                params: 
+                {
+                    statusValue: statusFilter||'',
+                    customerValue: customerFilter||'',
+                    subsidiaryValue: subsidiaryFilter||'',
+                    departmentValue: departmentFilter||''
+                }
+            });
+        }
+    }
+
+    /**
+     * Function to be executed when field is slaved.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     * @param {string} scriptContext.fieldId - Field name
+     *
+     * @since 2015.2
+     */
+    // function postSourcing(scriptContext) {
+
+    // }
+
+    /**
+     * Function to be executed after sublist is inserted, removed, or edited.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     *
+     * @since 2015.2
+     */
+    // function sublistChanged(scriptContext) {
+
+    // }
+
+    /**
+     * Function to be executed after line is selected.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     *
+     * @since 2015.2
+     */
+    // function lineInit(scriptContext) {
+
+    // }
+
+    /**
+     * Validation function to be executed when field is changed.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     * @param {string} scriptContext.fieldId - Field name
+     * @param {number} scriptContext.lineNum - Line number. Will be undefined if not a sublist or matrix field
+     * @param {number} scriptContext.columnNum - Line number. Will be undefined if not a matrix field
+     *
+     * @returns {boolean} Return true if field is valid
+     *
+     * @since 2015.2
+     */
+    // function validateField(scriptContext) {
+
+    // }
+
+    /**
+     * Validation function to be executed when sublist line is committed.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     *
+     * @returns {boolean} Return true if sublist line is valid
+     *
+     * @since 2015.2
+     */
+    // function validateLine(scriptContext) {
+
+    // }
+
+    /**
+     * Validation function to be executed when sublist line is inserted.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     *
+     * @returns {boolean} Return true if sublist line is valid
+     *
+     * @since 2015.2
+     */
+    // function validateInsert(scriptContext) {
+
+    // }
+
+    /**
+     * Validation function to be executed when record is deleted.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @param {string} scriptContext.sublistId - Sublist name
+     *
+     * @returns {boolean} Return true if sublist line is valid
+     *
+     * @since 2015.2
+     */
+    // function validateDelete(scriptContext) {
+
+    // }
+
+    /**
+     * Validation function to be executed when record is saved.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.currentRecord - Current form record
+     * @returns {boolean} Return true if record is valid
+     *
+     * @since 2015.2
+     */
+    // function saveRecord(scriptContext) {
+
+    // }
+
+    return {
+        // pageInit: pageInit,
+        fieldChanged: fieldChanged,
+        // postSourcing: postSourcing,
+        // sublistChanged: sublistChanged,
+        // lineInit: lineInit,
+        // validateField: validateField,
+        // validateLine: validateLine,
+        // validateInsert: validateInsert,
+        // validateDelete: validateDelete,
+        // saveRecord: saveRecord
+    };
+    
+});
---
+++ OTP-7432_GitHub_Training_1/src/FileCabinet/SuiteScripts/JobinandJismi/OTP-7490 CS & SL/jj_sl_so_status_filters_page_otp7490.js
@@ -0,0 +1,355 @@
+/**
+ * @NApiVersion 2.1
+ * @NScriptType Suitelet
+ */
+define(['N/record', 'N/search', 'N/ui/serverWidget'],
+    /**
+ * @param{record} record
+ * @param{search} search
+ * @param{serverWidget} serverWidget
+ */
+    (record, search, serverWidget) => {
+        /**
+         * Defines the Suitelet script trigger point.
+         * @param {Object} scriptContext
+         * @param {ServerRequest} scriptContext.request - Incoming request
+         * @param {ServerResponse} scriptContext.response - Suitelet response
+         * @since 2015.2
+         */
+        const onRequest = (scriptContext) => 
+        {
+            try
+            {            
+                if (scriptContext.request.method === 'GET') 
+                {
+                    let form = serverWidget.createForm({title: 'SalesOrder Filter Based on Status'});
+                    form.clientScriptFileId = 582;
+                    let statusForm = form.addField(
+                    { 
+                        id: 'custpage_status',
+                        type: serverWidget.FieldType.SELECT,
+                        label: 'Status'
+                    });
+                    statusForm.addSelectOption(
+                    {
+                        value: '',
+                        text: '' 
+                    });
+                    statusForm.addSelectOption(
+                    {
+                        value: 'SalesOrd:B',
+                        text: 'Pending Fulfillment' 
+                    });
+                    statusForm.addSelectOption(
+                    {
+                        value: 'SalesOrd:D',
+                        text: 'Partially Fulfilled' 
+                    });
+                    statusForm.addSelectOption(
+                    {
+                        value: 'SalesOrd:E',
+                        text: 'Pending Billing/Partially Fulfilled' 
+                    });
+                    statusForm.addSelectOption(
+                    {
+                        value: 'SalesOrd:F',
+                        text: 'Pending Billing' 
+                    });
+                    // let statusSearch = search.create({
+                    //     type: search.Type.SALES_ORDER,
+                    //     columns: ['statusref']
+                    // });
+                    // let statusSearchResult = statusSearch.run();
+                    // statusSearchResult.each(function (result) 
+                    // {
+                    //     let status = result.getText('statusref');
+                    //     let statusValue = result.getValue('statusref');
+                    //     statusForm.addSelectOption(
+                    //     {
+                    //         value: statusValue,
+                    //         text: status
+                    //     });
+                    //     return true;
+                    // });
+
+                    let customerForm = form.addField(
+                    { 
+                        id: 'custpage_customer',
+                        type: serverWidget.FieldType.SELECT,
+                        label: 'Customer',
+                        source:'customer'
+                    });
+                    // customerForm.addSelectOption(
+                    // {
+                    //     value: '',
+                    //     text: '' 
+                    // });
+                    // let customerSearch = search.create({
+                    //     type: search.Type.CUSTOMER,
+                    //     columns: ['entityid','companyname']
+                    // });
+                    // let customerSearchResult = customerSearch.run();
+                    // customerSearchResult.each(function (result) 
+                    // {
+                    //     let customer = result.getValue('entityid');
+                    //     let customerValue = result.getValue('companyname');
+                    //     customerForm.addSelectOption(
+                    //     {
+                    //         value: customerValue,
+                    //         text: customer
+                    //     });
+                    //     return true;
+                    // });
+
+                    let subsidiaryForm = form.addField(
+                    { 
+                        id: 'custpage_subsidiary',
+                        type: serverWidget.FieldType.SELECT,
+                        label: 'Subsidiary',
+                        source: 'subsidiary'
+                    });
+                    // subsidiaryForm.addSelectOption(
+                    // {
+                    //     value: '',
+                    //     text: '' 
+                    // });
+                    // let subsidiarySearch = search.create({
+                    //     type: search.Type.SUBSIDIARY,
+                    //     columns: ['name']
+                    // });
+                    // let subsidiarySearchResult = subsidiarySearch.run();
+                    // subsidiarySearchResult.each(function (result) 
+                    // {
+                    //     let subsidiary = result.getValue('name');
+                    //     subsidiaryForm.addSelectOption(
+                    //     {
+                    //         value: result.id,
+                    //         text: subsidiary
+                    //     });
+                    //     return true;
+                    // });
+
+                    let departmentForm = form.addField(
+                    { 
+                        id: 'custpage_department',
+                        type: serverWidget.FieldType.SELECT,
+                        label: 'Department',
+                        source: 'department'
+                    });
+                    // departmentForm.addSelectOption(
+                    // {
+                    //     value: '',
+                    //     text: '' 
+                    // });
+                    // let deptSearch = search.create({
+                    //     type: search.Type.DEPARTMENT,
+                    //     columns: ['name']
+                    // });
+                    // let deptSearchResult = deptSearch.run();
+                    // deptSearchResult.each(function (result) 
+                    // {
+                    //     let department = result.getValue('name');
+                    //     let deptValue = result.getValue('name');
+                    //     departmentForm.addSelectOption(
+                    //     {
+                    //         value: deptValue,
+                    //         text: department
+                    //     });
+                    //     return true;
+                    // });
+
+
+                    let soSublist = form.addSublist(
+                    {
+                        id: 'custpage_salesorder_sublist',
+                        type: serverWidget.SublistType.LIST,
+                        label: 'Sales Orders'
+                    });
+                    soSublist.addField(
+                    {
+                        id: 'custpage_internalid',
+                        type: serverWidget.FieldType.TEXT,
+                        label: 'Internal ID'
+                    });
+                    soSublist.addField(
+                    {
+                        id: 'custpage_docname',
+                        type: serverWidget.FieldType.TEXT,
+                        label: 'Document Number'
+                    });
+                    soSublist.addField(
+                    {
+                        id: 'custpage_date',
+                        type: serverWidget.FieldType.DATE,
+                        label: 'Date'
+                    });
+                    soSublist.addField(
+                    {
+                        id: 'custpage_status',
+                        type: serverWidget.FieldType.TEXT,
+                        label: 'Status'
+                    });
+                    soSublist.addField(
+                    {
+                        id: 'custpage_customer',
+                        type: serverWidget.FieldType.TEXT,
+                        label: 'Customer'
+                    });
+                    soSublist.addField(
+                    {
+                        id: 'custpage_subsidiary',
+                        type: serverWidget.FieldType.TEXT,
+                        label: 'Subsidiary'
+                    });
+                    soSublist.addField(
+                    {
+                        id: 'custpage_department',
+                        type: serverWidget.FieldType.TEXT,
+                        label: 'Department'
+                    });
+                    soSublist.addField(
+                    {
+                        id: 'custpage_class',
+                        type: serverWidget.FieldType.TEXT,
+                        label: 'Class'
+                    });
+                    // soSublist.addField(
+                    // {
+                    //     id: 'custpage_linenumber',
+                    //     type: serverWidget.FieldType.INTEGER,
+                    //     label: 'Line number'
+                    // });
+                    soSublist.addField(
+                    {
+                        id: 'custpage_subtotal',
+                        type: serverWidget.FieldType.CURRENCY,
+                        label: 'Subtotal'
+                    });
+                    soSublist.addField(
+                    {
+                        id: 'custpage_tax',
+                        type: serverWidget.FieldType.CURRENCY,
+                        label: 'Tax'
+                    });
+                    soSublist.addField(
+                    {
+                        id: 'custpage_total',
+                        type: serverWidget.FieldType.CURRENCY,
+                        label: 'Total'
+                    });
+
+                    let status = scriptContext.request.parameters.statusValue||'';
+                    let customer = scriptContext.request.parameters.customerValue||'';
+                    let subsidiary = scriptContext.request.parameters.subsidiaryValue||'';
+                    let department = scriptContext.request.parameters.departmentValue||'';
+
+                    statusForm.defaultValue = status;
+                    customerForm.defaultValue = customer;
+                    subsidiaryForm.defaultValue = subsidiary;
+                    departmentForm.defaultValue = department;
+
+                    let filter =[['mainline','is','T'],
+                                    'AND',
+                                    ['status', 'noneof', 'SalesOrd:A', 'SalesOrd:C', 'SalesOrd:G', 'SalesOrd:H']];
+                    if(status)
+                    {
+                        filter.push('AND',['status', 'is', status]);
+                    }
+                    if(customer)
+                    {
+                        filter.push('AND', ['entity', 'anyof', customer]);
+                    }
+                    if(subsidiary)
+                    {
+                        filter.push('AND', ['subsidiary', 'anyof', subsidiary]);
+                    }
+                    if(department)
+                    {
+                        filter.push('AND', ['department', 'anyof', department]);
+                    }
+    
+                    let soSearch = search.create({
+                        type: search.Type.SALES_ORDER,
+                        filters: filter,
+                        columns: ['internalid', 'tranid', 'trandate', 'statusref', 'entity', 'subsidiary', 'department', 'class', 'line', 'amount', 'taxamount', 'total']
+                    });
+                    let i=0;
+                    let resultSet = soSearch.run();
+                    resultSet.each(function (result)
+                    {
+                        soSublist.setSublistValue({
+                            id: 'custpage_internalid',
+                            line: i,
+                            value: result.getValue('internalid')
+                        });
+                        soSublist.setSublistValue({
+                            id: 'custpage_docname',
+                            line: i,
+                            value: result.getValue('tranid')
+                        });
+                        soSublist.setSublistValue({
+                            id: 'custpage_date',
+                            line: i,
+                            value: result.getValue('trandate')
+                        });
+                        soSublist.setSublistValue({
+                            id: 'custpage_status',
+                            line: i,
+                            value: result.getText('statusref')
+                        });
+
+                        soSublist.setSublistValue({
+                            id: 'custpage_customer',
+                            line: i,
+                            value: result.getText('entity')
+                        });
+                        soSublist.setSublistValue({
+                            id: 'custpage_subsidiary',
+                            line: i,
+                            value: result.getText('subsidiary')
+                        });
+                        soSublist.setSublistValue({
+                            id: 'custpage_department',
+                            line: i,
+                            value: result.getText('department') || null
+                        });
+                        soSublist.setSublistValue({
+                            id: 'custpage_class',
+                            line: i,
+                            value: result.getText('class') || null
+                        });
+                        // soSublist.setSublistValue({
+                        //     id: 'custpage_linenumber',
+                        //     line: i,
+                        //     value: result.getValue('line')
+                        // });
+                        soSublist.setSublistValue({
+                            id: 'custpage_subtotal',
+                            line: i,
+                            value: result.getValue('amount')|| null
+                        });
+                        soSublist.setSublistValue({
+                            id: 'custpage_tax',
+                            line: i,
+                            value: result.getValue('taxamount') || null
+                        });
+                        soSublist.setSublistValue({
+                            id: 'custpage_total',
+                            line: i,
+                            value: result.getValue('total')
+                        });
+                        i++;
+                        return true;
+                    });
+                    log.debug("Working");
+                    scriptContext.response.writePage(form);
+                }
+            }
+            catch(e){
+                log.error('Error found',e.message);
+            }
+        }
+
+        return {onRequest}
+
+    });
