9297f6a8451336cc22592192cd12b2ef4e32d909
Nathasha-jj <nathasha.vangana@jobinandjismi.com>
2024-08-14 13:07:48 +0530

    otp7496-fix: to fix bugs

--- OTP-7432_GitHub_Training_1/src/FileCabinet/SuiteScripts/JobinandJismi/OTP-7494 Business Requirement/jj_ue_email_overdue_so_salesmanager_otp7496.js
+++ OTP-7432_GitHub_Training_1/src/FileCabinet/SuiteScripts/JobinandJismi/OTP-7494 Business Requirement/jj_ue_email_overdue_so_salesmanager_otp7496.js
@@ -9,7 +9,7 @@
  *
  
  *
- * Author: Jobin & Jismi
+ * Author: Jobin & Jismi IT Services LLP
  *
  * Date Created : 1-August-2024
  *
@@ -35,9 +35,9 @@ define(['N/email', 'N/record', 'N/runtime'],
          * @param {ServletRequest} scriptContext.request - HTTP request information sent from the browser for a client action only.
          * @since 2015.2
          */
-        const beforeLoad = (scriptContext) => {
+        // const beforeLoad = (scriptContext) => {
 
-        }
+        // }
 
         /**
          * Defines the function definition that is executed before record is submitted.
@@ -47,9 +47,9 @@ define(['N/email', 'N/record', 'N/runtime'],
          * @param {string} scriptContext.type - Trigger type; use values from the context.UserEventType enum
          * @since 2015.2
          */
-        const beforeSubmit = (scriptContext) => {
+        // const beforeSubmit = (scriptContext) => {
 
-        }
+        // }
 
         /**
          * Defines the function definition that is executed after record is submitted.
@@ -61,50 +61,57 @@ define(['N/email', 'N/record', 'N/runtime'],
          */
         const afterSubmit = (scriptContext) => 
         {
-            if(scriptContext.type !== scriptContext.UserEventType.CREATE)
+            try
             {
-                return;
-            }
-            let newrecord = scriptContext.newRecord;
-            log.debug('New Record', newrecord);
-            let salesOrderId = newrecord.id;
-            log.debug('Sales Order Id', salesOrderId);
-            let customerId = newrecord.getValue('entity');
-            log.debug("Customer", customerId);
-            let customerName = newrecord.getText('entity');
-            log.debug("Customer", customerName);
-            let salesrepId = newrecord.getValue('salesrep');
-            log.debug("Sales Rep", salesrepId);
-            let currentUser = runtime.getCurrentUser();
-            let authorId = currentUser.id;
-            log.debug('Author Id',authorId);
-            let objRecCustomer = record.load(
-            {
-                type: record.Type.CUSTOMER,
-                id: customerId
-            });
-            let overdueAmount = objRecCustomer.getValue('overduebalance');
-            log.debug("OverDue Amount", overdueAmount);
-            let objRecSalesRep = record.load(
-            {
-                type: record.Type.EMPLOYEE,
-                id: salesrepId
-            });
-            let SalesRepSupervisorId = objRecSalesRep.getValue('supervisor');
-            log.debug('Sales RepSupervisor Id',SalesRepSupervisorId);
-            let emailBody = '<html><body><a href= https://td2924623.app.netsuite.com/app/accounting/transactions/salesord.nl?id='+salesOrderId+'&whence=>'+'Click Here to view the created Sales Order'+'</body></html>';
-            if(overdueAmount > 0)
-            {
-                email.send(
+                if(scriptContext.type !== scriptContext.UserEventType.CREATE)
                 {
-                    author: authorId,
-                    recipients: SalesRepSupervisorId,
-                    subject: "Sales Order Created for Customer with Overdue",
-                    body: 'Salesorder has been created for the customer: ' + customerName + ' with overdue amount: ' + overdueAmount +'<br/>'+ emailBody 
+                    return;
+                }
+                let newrecord = scriptContext.newRecord;
+                log.debug('New Record', newrecord);
+                let salesOrderId = newrecord.id;
+                log.debug('Sales Order Id', salesOrderId);
+                let customerId = newrecord.getValue('entity');
+                log.debug("Customer", customerId);
+                let customerName = newrecord.getText('entity');
+                log.debug("Customer", customerName);
+                let salesrepId = newrecord.getValue('salesrep');
+                log.debug("Sales Rep", salesrepId);
+                let currentUser = runtime.getCurrentUser();
+                let authorId = currentUser.id;
+                log.debug('Author Id',authorId);
+                let objRecCustomer = record.load(
+                {
+                    type: record.Type.CUSTOMER,
+                    id: customerId
                 });
+                let overdueAmount = objRecCustomer.getValue('overduebalance');
+                log.debug("OverDue Amount", overdueAmount);
+                let objRecSalesRep = record.load(
+                {
+                    type: record.Type.EMPLOYEE,
+                    id: salesrepId
+                });
+                let SalesRepSupervisorId = objRecSalesRep.getValue('supervisor');
+                log.debug('Sales RepSupervisor Id',SalesRepSupervisorId);
+                let emailBody = '<html><body><a href= https://td2924623.app.netsuite.com/app/accounting/transactions/salesord.nl?id='+salesOrderId+'&whence=>'+'Click Here to view the created Sales Order'+'</body></html>';
+                if(overdueAmount > 0)
+                {
+                    email.send(
+                    {
+                        author: authorId,
+                        recipients: SalesRepSupervisorId,
+                        subject: "Sales Order Created for Customer with Overdue",
+                        body: 'Salesorder has been created for the customer: ' + customerName + ' with overdue amount: ' + overdueAmount +'<br/>'+ emailBody 
+                    });
+                }
+            }
+            catch(e)
+            {
+                log.error("Error",e.message);
             }
         }
 
-        return {beforeLoad, beforeSubmit, afterSubmit}
+        return {afterSubmit}
 
     });
